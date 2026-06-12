3aff208c631fa07063c0b274b98240f46c0d6298
MuhabAlSadi <muhabalsadi23@gmail.com>
2026-06-12 21:28:10 +0300

    test coderabbit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
-import { Text, View } from 'react-native';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 justify-center items-center bg-gray-50">
+    <SafeAreaView className="flex-1 justify-center items-center bg-gray-50 p-5">
       <Text className="text-xl font-bold text-gray-800">Home Screen</Text>
       <Text className="text-orange-500 text-xl my-7 font-bold" > 3D Objects Printing Store</Text>
       <Text className="border border-orange-500 text-xl my-7 px-14 py-4 rounded-[10px]  ">Sign In</Text>
-    </View>
+    </SafeAreaView>
   );
 }
