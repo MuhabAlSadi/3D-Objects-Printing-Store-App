3c04700698db222b03b902d2cd2b85316a0b042b
MuhabAlSadi <muhabalsadi23@gmail.com>
2026-06-16 15:52:30 +0300

    Changing Tabs Bar design to Figma design and adding lucide-react-native package

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Text } from 'react-native';
+import { Text, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
@@ -6,7 +6,14 @@ export default function HomeScreen() {
     <SafeAreaView className="flex-1 justify-center items-center bg-gray-50 p-5">
       <Text className="text-xl font-bold text-gray-800">Home Screen</Text>
       <Text className="text-orange-500 text-xl my-7 font-bold" > 3D Objects Printing Store</Text>
-      <Text className="border border-orange-500 text-xl my-7 px-14 py-4 rounded-[10px]  ">Sign In</Text>
+      <TouchableOpacity className="border border-orange-500  my-7 px-14 py-4 rounded-[10px]  ">
+        <Text className="text-xl" >Sign In</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity>
+        <Text className="text-orange-500 text-xl my-7 font-bold rounded-full border border-orange-500 py-2 px-4" > View all</Text>
+      </TouchableOpacity>
+
     </SafeAreaView>
   );
 }
